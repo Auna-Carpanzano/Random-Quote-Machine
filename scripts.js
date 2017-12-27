b92d5806b5f11151b4767c89f71be7d4bfc5c123
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-26 18:13:20 -0700

    Create randomColor function that gets random number from 0-255 for variables r, g, and b

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-
+  function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+  }
 });
