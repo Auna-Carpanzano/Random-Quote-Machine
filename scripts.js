e5ebddd06b4ab4e5cdbabc033befe04747c6b85a
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-26 18:14:51 -0700

    Add return value of rgb string concatenated with variables r, g, and b

--- scripts.js
+++ scripts.js
@@ -3,5 +3,6 @@ $(document).ready(function() {
     var r = Math.floor(Math.random() * 256);
     var g = Math.floor(Math.random() * 256);
     var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
   }
 });
